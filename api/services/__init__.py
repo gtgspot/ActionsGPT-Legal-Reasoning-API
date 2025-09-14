66537afdb6ff275fa078007829de506ae6b3e191
gtgspot <github.tgspot@gmail.com>
2025-09-15 00:06:11 +1000

    Add quantum legal qubit model

---
+++ api/services/__init__.py
@@ -0,0 +1,7 @@
+"""Service constructors for quantum legal models."""
+
+from __future__ import annotations
+
+from .quantum_model import LegalQubit, from_probabilities
+
+__all__ = ["LegalQubit", "from_probabilities"]
